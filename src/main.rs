881959afdd22df83716243a4959ed9045f1f5369
Aiden <233508279+imOkayAiden@users.noreply.github.com>
2025-10-19 06:26:06 -0400

    added logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+
+
+fn add(a: i32, b: i32) -> i32{
+    a + b
+}
+
 fn main() {
-    println!("Hello, world!");
+    let  sum1: i32 = add(5,4);
+    let  sum2: i32 = add(6,923);
+    let  sum3: i32 = add(45,53);
+
+    println!("{}", sum1);
+    println!("{}", sum2);
+    println!("{}", sum3);
+    
 }
